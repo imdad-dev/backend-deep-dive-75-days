223a73bb85b7097bbbf77b94c57cf65e3e34d9b2
unknown <imdadulhoquekk@gmail.com>
2026-07-18 23:28:05 +0530

    Ref : improve project structure

--- typeScript/basic_config/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(person) {
-    return `Good Mornng ${person}`;
-}
-;
-const userName = "Imdadul";
-console.log(greet(userName));
-//# sourceMappingURL=index.js.map
--- typeScript/basic_config/dist/typeInTS.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=typeInTS.d.ts.map
